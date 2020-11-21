9fa3f35568bc43362ed954b7d839637d1a9dc05a
crunchypi <crunchypi@protonmail.com>
2020-11-21 15:19:11 +0100

    impl wikiapi subsearch into cli

--- src/data_gen/wikiapi.py
+++ src/data_gen/wikiapi.py
@@ -30,7 +30,7 @@ from src.typehelpers import ArticleData
 # // Used for pausing API requests to avoid spamming.
 # // Relying on this more than the wikipedia (mod)
 # // ratelimit setting.
-API_PAUSE_SEC = 1
+API_PAUSE_SEC = 0.2
 
 
 
@@ -56,21 +56,19 @@ def __pull(title:str, ttl=5)-> WikipediaPage:
 
 
 def pull_articles(titles:list, topics:list=[], subsearch:int=0): # // -> Gen
-    ''' Use a list of article <titles> to create
-        and return a generator which pulls
-        articles from wiki (API) and gives them
-        as src.typehelpers.ArticleData instances.
+    ''' Use a list of article <titles> to create and return a 
+        generator which pulls articles from wiki (API) and gives
+        them as src.typehelpers.ArticleData instances.
 
-        If <topics> is provided, then each yielded
-        ArticleData will get an attached topic --
-        the match is done by index. Example:
+        If <topics> is provided, then each yielded ArticleData 
+        will get an attached topic -- the match is done by index. 
+        Example:
             ArticleData of title[0] will have
             topics[0] as topic.
 
-        <subsearch> specifies how many branched
-        (recursive) searches to do, based on 
-        hyperlinks in each article. Ordered in
-        a DFS manner.
+        <subsearch> specifies how many branched (recursive) 
+        searches to do, based on hyperlinks in each article. 
+        Ordered in a DFS manner.
     '''
 
     for i, title in enumerate(titles):
